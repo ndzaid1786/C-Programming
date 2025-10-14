56be34536002c4f59f8c556f62daf6392d6bf1bc
ndzaid1786 <643zaidbadalpura@gmail.com>
2025-10-14 14:27:51 +0530

    Add files via upload

---
+++ experiment_2.c
@@ -0,0 +1,18 @@
+/*
+Name:Shaikh Zaid
+UIN:251P108       COMPUTER ENGINEERING - D2
+*/
+#include<stdio.h>
+int main(){
+     int lim;
+     int sum=0;
+     printf("enter the limit :");
+     scanf("%d",&lim);
+     for(int i=0;i<=lim;i++){
+        if(i%2!=0){
+            sum+=i;
+        }
+     }
+     printf("sum of odd is %d",sum);
+     return 0;
+}
